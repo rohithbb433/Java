fc25c7b61f613869d99a33922d36b8008a4d2ce3
Rohith <103022427+rohithbb433@users.noreply.github.com>
2023-07-24 18:05:22 +0530

    my name

---
+++ src/com/test/TestPractice2.java
@@ -0,0 +1,35 @@
+package com.test;
+
+
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.Repo.RepoClass;
+import com.Repo.SeleniumUtilClass;
+
+public class TestPractice2 extends SeleniumUtilClass {
+	RepoClass repoClass;
+	
+	@BeforeClass
+	public void preCondition() {
+		repoClass = new RepoClass();	
+	}
+
+	@Test
+	public void MainTest() {
+		repoClass.login();
+		
+	}
+	
+	
+	@AfterClass
+	public void postCondition() {
+		
+		//closeWebApplication();
+		
+		//test
+
+	}
+}
